765ef94701b4981480b3758d08712942192690c2
jemikanegara <jemikanegara@gmail.com>
2018-12-21 15:49:48 +0800

    add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { PhotosComponent } from './photos/photos.component';
+import { UserComponent } from './users/user/user.component';
+import { PhotoComponent } from './photos/photo/photo.component';
+import { AlbumComponent } from './albums/album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    UserComponent,
+    PhotoComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
